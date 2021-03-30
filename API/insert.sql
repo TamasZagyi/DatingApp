4659839a79026787f544bd341749716e7757c8c7
Tamas Zagyi <tamas.zagyi@gmail.com>
2021-03-30 10:23:34 +0200

    End of section 2

---
+++ API/insert.sql
@@ -0,0 +1,21 @@
+﻿-- Script Date: 3/29/2021 10:25 PM  - ErikEJ.SqlCeScripting version 3.5.2.87
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (1
+           ,"Bob");
+           
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (2
+           ,"Tom");
+           
+INSERT INTO [Users]
+           ([Id]
+           ,[UserName])
+     VALUES
+           (3
+           ,"Jane");
